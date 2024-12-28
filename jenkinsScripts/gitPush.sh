9c7d400135dbbebd1625cd946748a0e125fb9e83
j <76847923+jorbelca@users.noreply.github.com>
2024-12-28 16:29:17 +0100

    push v16

--- jenkinsScripts/gitPush.sh
+++ jenkinsScripts/gitPush.sh
@@ -8,7 +8,15 @@ MOTIU=$2
 # Mensaje del commit
 COMMIT_MESSAGE="Pipeline executada per ${EXECUTOR}. Motiu: ${MOTIU}"
 
-# Ejecutar los comandos de Git
+# Actualizar la rama local con el remoto
+echo "Actualizando la rama local con el remoto..."
+git pull origin ci_jenkins --rebase || { echo "Error durante git pull --rebase"; exit 1; }
+
+# Agregar cambios y hacer commit
+echo "Agregando cambios y haciendo commit..."
 git add README.md
 git commit -m "$COMMIT_MESSAGE" || echo "No hay cambios para commitear"
-git push origin ci_jenkins || echo "Error al hacer push, verifica conflictos"
+
+# Hacer push
+echo "Haciendo push de los cambios..."
+git push origin ci_jenkins || { echo "Error al hacer push, verifica conflictos"; exit 1; }
