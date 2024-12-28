77374f6e1927e0e8752facf0c917c8752af8f8c4
j <76847923+jorbelca@users.noreply.github.com>
2024-12-28 16:31:23 +0100

    push v17

--- jenkinsScripts/gitPush.sh
+++ jenkinsScripts/gitPush.sh
@@ -8,6 +8,13 @@ MOTIU=$2
 # Mensaje del commit
 COMMIT_MESSAGE="Pipeline executada per ${EXECUTOR}. Motiu: ${MOTIU}"
 
+# Verificar si hay cambios sin añadir o commitear
+if ! git diff --quiet || ! git diff --cached --quiet; then
+  echo "Detectados cambios no añadidos o sin commitear. Realizando commit temporal..."
+  git add .
+  git commit -m "Commit temporal: Resolviendo cambios antes de pull"
+fi
+
 # Actualizar la rama local con el remoto
 echo "Actualizando la rama local con el remoto..."
 git pull origin ci_jenkins --rebase || { echo "Error durante git pull --rebase"; exit 1; }
